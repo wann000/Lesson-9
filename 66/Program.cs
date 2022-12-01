33ce388d39a12f8bdb2e78b2a3f0816bd2993f56
wann000 <wann0@yandex.ru>
2022-12-02 01:52:41 +0300

    66

---
+++ 66/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetSum(int a, int b)
+{
+    if (a == b)
+    {
+        return a;
+    }
+    if (a > b)
+    {
+        return b + GetSum(a, b + 1);
+    }
+    return a + GetSum(a + 1, b);
+}
+Console.Clear();
+Console.Write("Введите первое число:");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число:");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetSum(n, m));
